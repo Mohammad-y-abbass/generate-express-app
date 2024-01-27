9f8d03165275f289cc0abdab9416484064fc4e73
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-01-27 16:29:57 +0200

    add apiError class

---
+++ template/apiError.js
@@ -0,0 +1,10 @@
+class apiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // This is to differentiate between operational errors and programming errors
+  }
+}
+
+module.exports = apiError;
